1be511083178156f4bcbb4e592a8b28be995716b
asbernar <54722592+asbernar@users.noreply.github.com>
2021-01-26 18:30:42 -0500

    Add files via upload

---
+++ StudyCards/StudyCards/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  StudyCards
+//
+//  Created by Bernard, Andrew S on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionlabel: UILabel! // ! means optional
+    @IBOutlet var answerlabel: UILabel!   //  @ means connect to UI
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
